2c09140e0f216c181c9d2db2fd3b613e0f59dd34
kafadarova <kafadarova@sendesignal.de>
2020-04-05 13:19:00 +0300

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model'
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // call the constructor
+    new Recipe('A Test Recipe', " This is simply a test", 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
