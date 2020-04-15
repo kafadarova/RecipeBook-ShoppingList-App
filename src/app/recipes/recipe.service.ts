39684d27312da569ed0a81b23251270626c6493d
kafadarova <k.kafadarova@gmail.com>
2020-04-15 22:08:54 +0300

    Use recipe service to improve cross-component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model'
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     // call the constructor
     new Recipe('A Test Recipe', " This is simply a test", 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/recipe-image-legacy-id-1074500_11.jpg'),
@@ -11,4 +15,5 @@ export class RecipeService {
     // return a exact copy of the recipe array (cant access the recipe array from outside)
     return this.recipes.slice();
   }
+
 }
