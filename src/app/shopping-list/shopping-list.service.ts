59e96e0380390d8e1e51d0536d5bd02056c0ecfc
kafadarova <k.kafadarova@gmail.com>
2020-04-16 12:30:19 +0300

    Create a shopping list service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,20 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+  ingredientsChanges = new EventEmitter<Ingredient[]>()
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    // Emit a new event and pass a copy of the original array
+    this.ingredientsChanges.emit(this.ingredients.slice());
+  }
+}
